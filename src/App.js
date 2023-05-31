73e5fb84ac638087ba721c22568078c6a2599191
Vishal Rajesh Karangale <karangalevr@gmail.com>
2023-05-31 19:12:35 +0530

    Adding Sign Up part

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSingUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './components/firebase/firebase.util';
+import { auth, creatUserProfileDocument } from './components/firebase/firebase.util';
 
 
 
@@ -21,9 +21,22 @@ class App extends React.Component{
   unsubscribeFromAuth = null;
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth){
+        const userRef = await creatUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapshot => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data(),
+            }
+          })
+        });
+      }
+      else {
+        this.setState({currentUser: null})
+      }
+    });
   }
 
   componentWillUnmount(){
