5352049331234f413a14474a24bc340bb6fa1686
Vishal Rajesh Karangale <karangalevr@gmail.com>
2023-06-01 13:01:58 +0530

    add to cart functionality

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -32,7 +32,7 @@ const Header = ({ currentUser, hidden }) => (
         <CartIcon />
       </div>
       {
-        hidden ? null : <CartDropdown />
+        hidden ? null  : <CartDropdown />
       }
     </div>
   );
--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -3,3 +3,8 @@ import { CartActiontypes } from "./cart.types";
 export const toggleCartHidden = () => ({
     type: CartActiontypes.TOGGLE_CART_HIDDEN
 });
+
+export const addItem = item => ({
+    type: CartActiontypes.ADD_ITEM,
+    payload: item
+});
