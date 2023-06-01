e91dd9c6f0aac4987203ba5c4269c234deab7aba
Vishal Rajesh Karangale <karangalevr@gmail.com>
2023-06-01 17:00:16 +0530

    added remove item from cart button

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -8,3 +8,8 @@ export const addItem = item => ({
     type: CartActiontypes.ADD_ITEM,
     payload: item
 });
+
+export const clearItemFromCart = item => ({
+    type: CartActiontypes.CLEAR_ITEM_FROM_CART,
+    payload: item
+});
