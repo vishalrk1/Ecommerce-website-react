e431a0def54399940ddda4e04a094708702cc194
Vishal Rajesh Karangale <59719046+vishalrk1@users.noreply.github.com>
2023-08-29 18:08:02 +0530

    Update firebase.util.js
    Signed-off-by: Vishal Rajesh Karangale <59719046+vishalrk1@users.noreply.github.com>

--- src/components/firebase/firebase.util.js
+++ src/components/firebase/firebase.util.js
@@ -2,7 +2,15 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 
-const config = 
+const config = {
+    apiKey: "AIzaSyDUsOGg8ovsbw0T5nGC5R4ynCJ6Q6R86GI",
+    authDomain: "crwn-db-92c64.firebaseapp.com",
+    projectId: "crwn-db-92c64",
+    storageBucket: "crwn-db-92c64.appspot.com",
+    messagingSenderId: "446177239272",
+    appId: "1:446177239272:web:2ad46bdba40747c198e95d",
+    measurementId: "G-MR0C9CZ4YN"
+};
 
 export const creatUserProfileDocument = async (userAuth, additionalData) => {
     if(!userAuth) return;
